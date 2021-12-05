c2844619359807470b0628442a6e9dc38faca102
Jiale (Jerry) Chen <75757467+jchen056@users.noreply.github.com>
2021-12-04 22:46:15 -0500

    Update pde13.R

--- pde13.R
+++ pde13.R
@@ -1,14 +1,18 @@
-#u_t=u_xx+u_x x:[0,1];t:[0,0.02]
+#u_t=u_xx+u_x 
+#x:[0,1];t:[0,0.02]
 #epsx=0.1, epst-0.05
 x=seq(from=0,to=1,by=0.1)
+ncols=length(x)
 t=seq(from=0,to=0.02,by=0.005)
-A=matrix(NA,nrow=5,ncol=11)
+nrows=length(t)
+A=matrix(NA,nrow=nrows,ncol=ncols)
 A[1,]=sqrt(x*(1-x))
 A[,1]=0
-A[,11]=0
-for(i in 2:5){
-  for (j in 2:10){
-    A[i,j]=0*A[i-1,j]+0.525*A[i-1,j-1]+0.475*A[i-1,j+1]
+A[,ncols]=0
+for(i in 2:nrows){
+  for (j in 2:(ncols-1)){
+    A[i,j]=0*A[i-1,j]+0.475*A[i-1,j-1]+0.525*A[i-1,j+1]
   }
 }
-persp(t,x,A)
+persp(t,x,A)#length(x):nrow of A
+A
