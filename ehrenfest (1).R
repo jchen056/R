91b11fd7e45abd97b347d9107a132512e424bfd2
Jiale (Jerry) Chen <75757467+jchen056@users.noreply.github.com>
2022-03-25 09:03:45 -0400

    Add files via upload

---
+++ ehrenfest (1).R
@@ -0,0 +1,30 @@
+A = rbind( c(0,1,0,0,0,0) , 
+           c(1/5,0,4/5,0,0,0),
+           c(0,2/5,0,3/5,0,0),
+           c(0,0,3/5,0,2/5,0),
+           c(0,0,0,4/5,0,1/5),
+           c(0,0,0,0,1,0)
+           )
+
+number = c("0","1","2","3","4","5")
+#there are two boxes, box1 and box2; box1 is of our interest
+#pick a particle(only one), move it to other box
+
+rownames(A) = number
+colnames(A) = number
+
+x = 1
+for(i in 1:1000){
+  for(j in 1:6){
+    if(x[i] == j){#what is the current state
+      x = c(x,sample(1:6,size=1,prob=A[j,]))
+    }
+  }
+}
+
+plot(x)
+
+#raise the matrix to a power
+#p^l is a matrix of transitioning between states when your path has length l
+A%*%A
+(A%*%A)%*%A
